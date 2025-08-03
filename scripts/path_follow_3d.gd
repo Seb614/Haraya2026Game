2ccc4c1b475d907dad2c108d2aed688a3240bb48
Jackkauffnotrades <b2027ajgonzales@pshs.edu.ph>
2025-08-04 06:24:50 +0800

    holy shit

--- scripts/path_follow_3d.gd
+++ scripts/path_follow_3d.gd
@@ -1,9 +1,8 @@
 extends PathFollow3D
 
-var speed = 2
+var speed = 0.3
 
 	
 func _process(delta):
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		progress += delta * speed * direction
+	progress += delta * speed 
+	speed += delta / 6 * 2
