032bb99550972981364a307863e3d3a7de6f4894
Seb <b2027stabainza@pshs.edu.ph>
2025-08-19 11:34:22 +0800

    door fix

--- scripts/doorway.gd
+++ scripts/doorway.gd
@@ -2,20 +2,24 @@ extends Area2D
 
 @export var target_scene: String
 @export var travel_dir: int
+@export var msg : String
 
 var interactable = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_body_entered(body):
-	interactable = true
+	if body.name == "Player":
+		interactable = true
 	
 func _on_body_exited(body):
-	interactable = false
+	if body.name == "Player":
+		interactable = false
 	
 func _process(delta):
 	if Input.is_action_just_pressed("interact"):
 		if interactable:
 			print("Entered ", travel_dir)
+			print(msg)
 			GlobalData.travel_dir = travel_dir
 			get_tree().change_scene_to_file("res://scenes/screens/" + target_scene)
