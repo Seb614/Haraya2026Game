b71b444a754a0ba62e82bc783a91fadd6cae57d7
Jackkauffnotrades <b2027ajgonzales@pshs.edu.ph>
2025-10-31 09:59:28 +0800

    teka muna

--- scenes/objects/bgfade.gd
+++ scenes/objects/bgfade.gd
@@ -1,12 +1,12 @@
 extends AudioStreamPlayer
 @export var audio : AudioStream
-@export var seek : float
-var stop
+@export var seekto : float
+
 func _ready() -> void:
 	self.stream = audio
 	if self.autoplay:
-		self.play()
+		self.play(seekto)
 	await Fade.on_transition_start
-	stop = self.get_playback_position() + 0.5
+	
 	var tween = create_tween()
 	tween.tween_property(self, "volume_db", -50, 0.5)
